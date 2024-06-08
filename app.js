5ac7b72c3f3f99d2502701ef687b5a007a3e813b
rajeev-k47 <164878192+rajeev-k47@users.noreply.github.com>
2024-06-08 22:39:22 +0530

    Update app.js
    commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const port = 3000;
+
+const CLIENT_ID = '448403106695-qmmbjaj8f5ns5r3kg45asne0cg3g0ngt.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'https://cred-test.onrender.com/callback';
 
 app.get('/.well-known/assetlinks.json', (req, res) => {
     const jsonData = [{
@@ -18,6 +23,41 @@ app.get('/.well-known/assetlinks.json', (req, res) => {
     //res.setHeader('Content-Type', 'application/json');
     //res.status(200).send(JSON.stringify(jsonData, null, 2)); 
     res.json(jsonData);
+});
+app.get('/callback', async (req, res) => {
+    const { code, state } = req.query;
+    console.log('hello')
+    
+
+    if (code) {
+        try {
+            // Exchange the authorization code for an access token
+            console.log(code)
+            const response = await axios.post('https://oauth2.googleapis.com/token', {
+                code: code,
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                redirect_uri: REDIRECT_URI,
+                grant_type: 'authorization_code'
+            });
+
+            const { access_token, refresh_token, expires_in } = response.data;
+
+            // Save the tokens securely (e.g., in a database)
+            // For demonstration, we are just sending the tokens in the response
+            res.json({
+                access_token,
+                refresh_token,
+                expires_in
+            });
+
+        } catch (error) {
+            console.error('Error exchanging code for token:', error);
+            res.status(500).send('Authentication failed');
+        }
+    } else {
+        res.status(400).send('No code provided');
+    }
 });
 
 app.get('/index.html',(req,res)=>{
